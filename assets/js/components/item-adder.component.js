33d63ae5be09731dad0d25bfef5c27b52e5fb580
jarod <jarodccrowe@gmail.com>
2019-05-17 14:16:56 +1000

    Add nicer ux for item selectors

--- assets/js/components/item-adder.component.js
+++ assets/js/components/item-adder.component.js
@@ -7,7 +7,6 @@
  * @slot default                     [form contents]
  *
  * @event update:cloudError          [.sync]
- * @event update:syncing             [.sync]
  * @event update:formErrors          [.sync]
  * @event submitted                  [emitted after the server responds with a 2xx status code]
  * @event rejected                   [emitted after the server responds with a non-2xx status code]
@@ -17,18 +16,19 @@
 parasails.registerComponent('itemAdder', {
 
   props: [
-    'syncing',
     'cloudError',
     'product',
     'action',
     'index',
     'handle-submitting',
+    'syncing',
   ],
 
   data: function () {
     return {
       quantity: '',
       productModalVisable: false,
+      formErrors: {},
     };
   },
 
@@ -56,13 +56,17 @@ parasails.registerComponent('itemAdder', {
           placeholder="0"
           autocomplete="quantity"
           v-model="quantity"
+          :class="[formErrors.Quantity ? 'is-invalid' : '']"
         />
-        <button
-          @click="submit"
-          class="btn btn-block btn-outline-primary btn-sm"
-          :class="[syncing ? 'syncing' : '']"
+        <div
+          class="invalid-feedback"
+          v-if="formErrors.Quantity"
+          style="margin-top: -15px; margin-bottom: 5px;"
         >
-          <span class="button-text" v-if="!syncing"><slot name="default">Add to Order</slot></span>
+          Please enter a valid quantity.
+        </div>
+        <button @click="submit" type="submit" class="btn btn-block btn-outline-primary ajax-button" :class="[syncing ? 'syncing' : '']">
+          <span class="button-text" v-if="!syncing"><slot name="default">Add to Cart</slot></span>
           <span class="button-loader clearfix" v-if="syncing">
             <slot name="syncing-state">
               <div class="loading-dot dot1"></div>
@@ -110,8 +114,8 @@ parasails.registerComponent('itemAdder', {
   `,
 
   mounted: async function (){
-    console.log(this.product);
-    console.log(this.index);
+    // console.log(this.product);
+    // console.log(this.index);
   },
 
   methods: {
@@ -126,13 +130,23 @@ parasails.registerComponent('itemAdder', {
     },
 
     submit: async function () {
-      console.log('submit ' + this.quantity + ' ' + this.product.id);
       if (this.syncing) {
         return;
       }
 
+      // handle parsing
+      this.formErrors = {};
+      if (!this.quantity) {
+        this.formErrors.Quantity = true;
+      }
+      if (this.quantity === '' || this.quantity === 0) {
+        this.formErrors.Quantity = true;
+      }
+      if (Object.keys(this.formErrors).length > 0) {
+        return;
+      }
+
       this.$emit('update:cloudError', '');
-      this.$emit('update:syncing', true);
 
       var failedWithCloudExit;
       var result;
@@ -140,24 +154,29 @@ parasails.registerComponent('itemAdder', {
         result = await this.handleSubmitting({
           Id: this.product.id,
           Quantity: this.quantity,
-        })} catch (err) {
-          rawErrorFromCloudSDK = err;
-          if (_.isString(err)) {
-            failedWithCloudExit = err || 'error';
-          } else if (_.isError(err)) {
-            failedWithCloudExit = err.exit || 'error';
-          } else if (_.isObject(err)) {
-            failedWithCloudExit = Object.keys(err)[0] || 'error';
-          } else {
-            throw err;
-          }
+        })
+      } catch (err) {
+        rawErrorFromCloudSDK = err;
+        if (_.isString(err)) {
+          failedWithCloudExit = err || 'error';
+        } else if (_.isError(err)) {
+          failedWithCloudExit = err.exit || 'error';
+        } else if (_.isObject(err)) {
+          failedWithCloudExit = Object.keys(err)[0] || 'error';
+        } else {
+          throw err;
         }
+      }
 
-      this.$emit('update:syncing', false);
       if (failedWithCloudExit) {
         this.$emit('update:cloudError', failedWithCloudExit);
       }
 
+      if (!failedWithCloudExit) {
+        this.$emit('submitted', result);
+      } else {
+        this.$emit('rejected', rawErrorFromCloudSDK);
+      }
     }
   },
 })
