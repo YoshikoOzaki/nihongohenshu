ae4cdf376cd5ac948b6d6a89df2c8a04aea2d342
jarod <jarodccrowe@gmail.com>
2019-08-14 11:09:01 +1000

    ie testing

--- tasks/register/build.js
+++ tasks/register/build.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [
     'polyfill:dev', //« uncomment to ALSO transpile during development (for broader browser compat.)
     'compileAssets',
-    'babel',        //« uncomment to ALSO transpile during development (for broader browser compat.)
+    'babel:dev',        //« uncomment to ALSO transpile during development (for broader browser compat.)
     'linkAssetsBuild',
     'clean:build',
     'copy:build'
